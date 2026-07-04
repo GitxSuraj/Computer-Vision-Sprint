90dd129bc91bcc6f0a0f8b2b0f51a0aa3e710f4b
unknown <sharmasurajbxr@gmail.com>
2026-07-04 12:52:31 +0530

    Added comments for better understanding

--- CNN/seeFeatures.py
+++ CNN/seeFeatures.py
@@ -5,17 +5,21 @@ import torchvision.transforms as transforms
 from PIL import Image
 import matplotlib.pyplot as plt
 
-img_dog = Image.open("images/test_dog.jpg")
-img_me = Image.open("images/test.jpg")
+img_dog = Image.open("images/test_dog.jpg") #Testing on a Dog's Image
+img_me = Image.open("images/test.jpg") #Testing on my Image
 
-model = models.vgg16(weights=models.VGG16_Weights.DEFAULT).eval()
+model = models.vgg16(weights=models.VGG16_Weights.DEFAULT).eval() #Loading model with default weight, 
+# in evaluation mode (.eval()) so that it doesn't start storing info and learning
 
-firstConvLayer = model.features[0]
+firstConvLayer = model.features[0] #It's Conv2d layer, A model has multiple features layer, selcting the index feature for this evalution
 preprocess = transforms.Compose([
-    transforms.Resize((224,224)), transforms.ToTensor(),transforms.Normalize(mean=[0.485,0.456,0.406], std = [0.229, 0.224, 0.225])
+    transforms.Resize((224,224)), 
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485,0.456,0.406], std = [0.229, 0.224, 0.225])
 ])
 inputTensorDog = preprocess(img_dog).unsqueeze(0)
 inputTensorMe = preprocess(img_me).unsqueeze(0)
+#.unsqueeze(0) adds batch size convers it from (C,W,H) to (Batch Size, C, W, H) with batch size = 1, i.e 1 image
 
 with torch.no_grad():
     featureMapsDog = firstConvLayer(inputTensorDog)
@@ -28,6 +32,11 @@ fig.suptitle("Real-Time Visualization: First 16 Convolutional Channels", fontsiz
 
 mapsDog = featureMapsDog.squeeze(0).cpu().numpy()
 mapsMe = featureMapsMe.squeeze(0).cpu().numpy()
+"""
+.squeeze(0) - removes that extra dimenstion of batch size, converts back to (C,W,H)
+.cpu() - brings the data from gpu to cpu so that matplotlib can read it
+.numpy() - converts from tensor to numpy
+"""
 show = int(input("Press 1 for dog and 0 for human: "))
 if show == 1:
     print("\nFor Dog Image\n")
